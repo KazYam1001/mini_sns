92d180122da2b32912253684c8b1729fadc50eb7
Kazushi Yamanishi <keironia187149@gmail.com>
2019-09-21 13:22:39 +0900

    add routing for wizard signup form

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
 Rails.application.routes.draw do
+  root 'users#index'  # ログインor新規登録を選ぶページ
   devise_for :users, controllers: {
     omniauth_callbacks: 'users/omniauth_callbacks',
     registrations: 'users/registrations'
   }
-  root 'users#index'  # ログインor新規登録を選ぶページ
+  devise_scope :user do
+    get '/users/basic_info' => 'users/registrations#basic_info', as: :new_basic_info
+    post '/users/basic_info' => 'users/registrations#add_basic', as: :add_basic
+  end
   resources :users, only: :new
 end
